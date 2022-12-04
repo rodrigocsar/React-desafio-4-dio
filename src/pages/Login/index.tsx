f0ce579d4523c66fa3f386fb2ca21c3aff93d4af
rodrigo <rodrigocesar068@gmail.com>
2022-12-03 22:27:05 -0300

    Desafio react 04

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,19 +1,19 @@
-import { useForm } from "react-hook-form";
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { useForm } from 'react-hook-form';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
-import { defaultValues, IFormLogin } from "./types";
+import { Container, LoginContainer, Column, Spacing, Title } from './styles';
+import { defaultValues, IFormLogin } from './types';
 
 const schema = yup
   .object({
-    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    email: yup.string().email('E-mail inválido').required('Campo obrigatório'),
     password: yup
       .string()
-      .min(6, "No minimo 6 caracteres")
-      .required("Campo obrigatório"),
+      .min(6, 'No minimo 6 caracteres')
+      .required('Campo obrigatório'),
   })
   .required();
 
@@ -23,10 +23,14 @@ const Login = () => {
     formState: { errors, isValid },
   } = useForm<IFormLogin>({
     resolver: yupResolver(schema),
-    mode: "onBlur",
+    mode: 'onBlur',
     defaultValues,
-    reValidateMode: "onChange",
+    reValidateMode: 'onChange',
   });
+
+  const mensagemLogin = () => {
+    alert('Login realizado com sucesso!!!');
+  };
 
   return (
     <Container>
@@ -35,21 +39,21 @@ const Login = () => {
           <Title>Login</Title>
           <Spacing />
           <Input
-            name="email"
-            placeholder="Email"
+            name='email'
+            placeholder='Email'
             control={control}
             errorMessage={errors?.email?.message}
           />
           <Spacing />
           <Input
-            name="password"
-            type="password"
-            placeholder="Senha"
+            name='password'
+            type='password'
+            placeholder='Senha'
             control={control}
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title='Entrar' disabled={!isValid} onClick={mensagemLogin} />
         </Column>
       </LoginContainer>
     </Container>
